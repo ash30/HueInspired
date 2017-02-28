413b7bde5a1a2a94320fcfc56555e6e5d603839a
ashley arthur <ashleyarthur98@gmail.com>
2017-02-28 08:23:27 +0000

    manual CDSImageSource subclass

---
+++ CDSImageSource.swift
@@ -0,0 +1,42 @@
+//
+//  ImageSource.swift
+//  HueInspired
+//
+//  Created by Ashley Arthur on 24/02/2017.
+//  Copyright © 2017 AshArthur. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(CDSImageSource)
+public final class CDSImageSource: NSManagedObject, CustomManagedObject {
+    
+    static let entityName: String = "CDSImageSource"
+    
+    @NSManaged public internal(set) var imageData: Data?
+    @NSManaged public internal(set) var externalID: String
+    @NSManaged public internal(set) var palette: CDSColorPalette?
+    
+}
+
+
+extension CDSImageSource {
+    
+    convenience init(context:NSManagedObjectContext, id:String, palette: CDSColorPalette?, imageData:Data?){
+        
+        if let description = NSEntityDescription.entity(forEntityName: CDSImageSource.entityName, in: context) {
+            self.init(entity: description, insertInto: context)
+            
+            self.imageData = imageData
+            self.externalID = id
+            self.palette = palette
+
+        }
+            
+        else {
+            fatalError("Unknown Entity Description")
+        }
+    }
+    
+}
