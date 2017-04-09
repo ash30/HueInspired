f72f7a7c1dfbcf003aa9c4e02ccdf5d4f1267d02
ashley arthur <ashleyarthur98@gmail.com>
2017-04-09 23:03:25 +0100

    Feature: Implement LAB -> SRGB

--- HueInspired/ColorExtract/ColorSpaces.swift
+++ HueInspired/ColorExtract/ColorSpaces.swift
@@ -18,8 +18,21 @@ fileprivate let SRGB_WORKINGSPACE = matrix_double3x3(
     )
 )
 
+fileprivate let INVERSE_SRGB_WORKINGSPACE = matrix_double3x3(
+    columns:(
+        vector_double3(3.2404542, -0.9692660, 0.0556434),
+        vector_double3(-1.5371385, 1.8760108, -0.2040259),
+        vector_double3(-0.4985314, 0.0415560, 1.0572252)
+    )
+)
+
+
 func inverseCompandingSRGB(_ value:Double) -> Double {
     return value <= 0.04045 ? (value / 12.92) : pow((value + 0.055) / 1.055, 2.4)
+}
+
+func compandingSRGB(_ value:Double) -> Double {
+    return value <= 0.0031308 ? 12.92 * value : (1.055 * pow(value, 1/2.4)) - 0.055
 }
 
 func SRGB_TO_XYZ(_ vec: vector_double3) -> vector_double3 {
@@ -51,4 +64,38 @@ func XYZ_TO_CIELAB(_ vec: vector_double3, referenceWhite:vector_double3) -> vect
     
     return vector_double3([l,a,b])
 }
+
+func CIELAB_TO_XYZ(_ vec: vector_double3, referenceWhite:vector_double3) -> vector_double3 {
+    
+    let e = 0.008856
+    let k = 903.3
+    
+    let fy = (vec.x + 16) / 116
+    let fz = fy - (vec.z / 200)
+    let fx = (vec.y / 500 ) + fy
+    
+    let x = pow(fx,3.0) > e ? pow(fx,3.0) : ((116 * fx) - 16) / k
+    let y = vec.x > (k*e) ? pow((vec.x + 16) / 116, 3.0) : vec.x / k
+    let z = pow(fz, 3.0) > e ? pow(fz, 3.0) : ((116 * fz) - 16) / k
+    
+    return vector_double3([
+        x * referenceWhite.x,
+        y * referenceWhite.y,
+        z * referenceWhite.z
+    ])
+}
+
+func XYZ_TO_SRGB(_ vec: vector_double3) -> vector_double3 {
+
+    let rbg = matrix_multiply(INVERSE_SRGB_WORKINGSPACE, vec)
+    return vector3(
+        compandingSRGB(rbg.x),
+        compandingSRGB(rbg.y),
+        compandingSRGB(rbg.z)
+    )
+    
+}
+
+
+
 
