9e8bb76518060698a6625ef928ec822d95e9f493
ashley arthur <ashleyarthur98@gmail.com>
2017-07-17 17:07:38 +0100

    Enhance: String to Base64
    Simple extension to provide method on string

---
+++ HueInspired/String+Base64Encode.swift
@@ -0,0 +1,25 @@
+//
+//  String+Base64Encode.swift
+//  HueInspired
+//
+//  Created by Ashley Arthur on 17/07/2017.
+//  Copyright © 2017 AshArthur. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    func fromBase64() -> String? {
+        guard let data = Data(base64Encoded: self) else {
+            return nil
+        }
+        
+        return String(data: data, encoding: .utf8)
+    }
+    
+    func toBase64() -> String {
+        return Data(self.utf8).base64EncodedString()
+    }
+    
+}
