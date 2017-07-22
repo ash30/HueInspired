a6105fd0a844615ed5216d33604128a3419bc4a7
ashley arthur <ashleyarthur98@gmail.com>
2017-07-22 17:34:57 +0100

    Refactor: Palette Creation delegate to use new factory method

--- HueInspired/ViewController/RootViewController/RootViewControllerPaletteCreatorDelegate.swift
+++ HueInspired/ViewController/RootViewController/RootViewControllerPaletteCreatorDelegate.swift
@@ -43,98 +43,59 @@ class RootViewControllerPaletteCreatorDelegate: PaletteCreatorDelegate {
     
     func didSelectUserImage(creator:UIViewController, image: UIImage, id:String){
         
-        // Setup View controller to transition to
-        
+        // Setup Container VC
+
         let containerVC =  ActionContainer()
-        containerVC.action = { _ in 
-            // Bit of Hack, we want to dismiss image picker + detailer, so we reach back...
-            creator.presentingViewController?.dismiss(animated: true, completion: nil)
-        }
+        containerVC.actionButtonText = "Save"
         containerVC.navigationItem.leftBarButtonItem = UIBarButtonItem(
             barButtonSystemItem: .cancel, target: creator, action: #selector(creator.targetDismiss)
         )
         
-        containerVC.actionButtonText = "Save"
-        
-        let navVC = UINavigationController()
-        navVC.viewControllers = [containerVC]
-        
         // Setup Palette Detail
+
         let paletteVC = PaletteDetailViewController()
         containerVC.addChildViewController(paletteVC)
         
-        _ = createPaletteFromUserImage(image: image, id: id).then { [weak paletteVC, weak self] (p:ColorPalette) -> () in
+        _ = createPaletteFromUserImage(image: image, id: id).then { [weak containerVC, weak paletteVC, weak self] (p:ColorPalette) -> () in
             guard
                 let _self = self,
                 let dataSource = _self.factory(p) else {
-                throw PaletteErrors.paletteCreationFailure
+                    throw PaletteErrors.paletteCreationFailure
             }
             paletteVC?.dataSource = dataSource
+            
+            containerVC?.action = { vc in
+                do {
+                    try dataSource.save()
+                    // Bit of Hack, we want to dismiss image picker + detailer, so we reach back...
+                    creator.presentingViewController?.dismiss(animated: true, completion: nil)
+                }
+                catch let error as NSError {
+                    guard
+                        error.code == 133021,
+                        error.domain == "NSCocoaErrorDomain",
+                        let conflicts = error.userInfo["conflictList"] as? [NSConstraintConflict]
+                        else {
+                            vc.report(error: error)
+                            return // Unknown error ...
+                    }
+                    vc.showErrorAlert(title: "Duplicate palette", message: "Palette Already exists in your collection")
+                }
+            }
         }
-            .catch { [weak paletteVC] (e:Error) in
-            paletteVC?.report(error: e)
+        .catch { [weak paletteVC] (e:Error) in
+                paletteVC?.report(error: e)
         }
         
+        
+        let navVC = UINavigationController()
+        navVC.viewControllers = [containerVC]
         navVC.modalTransitionStyle = .flipHorizontal
         creator.present(navVC, animated: true)
         
         
         
     }
-    
-    
-//    func createPaletteFromUserImage(ctx:NSManagedObjectContext, image: UIImage, id:String) -> Promise<NSManagedObjectID> {
-//        
-//        let p = Promise<NSManagedObjectID>.pending()
-//        
-//        ctx.perform  {
-//            
-//            guard
-//                let palette = ImmutablePalette(withRepresentativeSwatchesFrom: image, name: nil, guid:id)
-//                else {
-//                    p.reject(PaletteErrors.paletteCreationFailure)
-//                    return
-//            }
-//            do{
-//                let paletteEntity = CDSColorPalette(context: ctx, palette: palette)
-//                try ctx.save()
-//                p.fulfill(paletteEntity.objectID)
-//            }
-//            catch let error as NSError {
-//                // This may fail due to palette existing already!
-//                // If so we fulfil promise with existing
-//                
-//                // TODO: Proper Constants for error
-//                
-//                guard
-//                    error.code == 133021,
-//                    error.domain == "NSCocoaErrorDomain",
-//                    let conflicts = error.userInfo["conflictList"] as? [NSConstraintConflict],
-//                    conflicts.count == 1
-//                    else {
-//                        // Unrecoginised error
-//                        p.reject(error)
-//                        return
-//                }
-//                
-//                // TODO: Currently we can assume a one to one map
-//                // between palette and image source. This may not hold
-//                // forever...
-//                
-//                guard
-//                    let original_imagesource = conflicts[0].databaseObject as? CDSImageSource,
-//                    let original_palette = original_imagesource.palette
-//                    
-//                    else {
-//                        // can't recover from the error...
-//                        p.reject(error)
-//                        return
-//                }
-//                p.fulfill(original_palette.objectID)
-//            }
-//        }
-//        return p.promise
-//    }
     
 }
 
