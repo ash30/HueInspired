4830fd7d3ebb5fa4d26f2126b6334bfd8ad29c22
ashley arthur <ashleyarthur98@gmail.com>
2017-07-22 21:39:31 +0100

    Tests: Adding Inverse colorspace conversion tests

--- HueInspiredTests/ColorSpaceTests.swift
+++ HueInspiredTests/ColorSpaceTests.swift
@@ -11,7 +11,7 @@ import simd
 @testable import HueInspired
 
 
-class ColorSpaceConversionCIELABTests: XCTestCase {
+class ColorSpaceConversionSRGBTests: XCTestCase {
     
     let whitePoint = vector_double3(0.95047, 1.00, 1.08883)
     let tolerance = 0.1
@@ -21,11 +21,16 @@ class ColorSpaceConversionCIELABTests: XCTestCase {
         return XYZ_TO_CIELAB(colorXYZ, referenceWhite: whitePoint)
     }
     
+    func mapLAB_TO_SRGB(_ a: vector_double3) -> vector_double3{
+        let colorXYZ = CIELAB_TO_XYZ(a, referenceWhite: whitePoint)
+        return XYZ_TO_SRGB(colorXYZ)
+    }
+    
     /*
-        Test color conversion between sRGB space to LAB
+        Test color conversion between sRGB space to LAB and Back again
     */
     
-    func testZero() {
+    func testZero_SRGB_TO_LAB() {
         let colorSRGB = vector_double3(0,0,0)
         let referenceLAB = vector_double3(0,0,0)
         let result = mapSRGB_TO_LAB(colorSRGB)
@@ -36,7 +41,18 @@ class ColorSpaceConversionCIELABTests: XCTestCase {
         
     }
     
-    func testMidpoint() {
+    func testZero_LAB_TO_SRGB() {
+        let referenceSRGB = vector_double3(0,0,0)
+        let colorLAB = vector_double3(0,0,0)
+        let result = mapLAB_TO_SRGB(colorLAB)
+        
+        XCTAssertEqualWithAccuracy(result.x, referenceSRGB.x, accuracy:tolerance)
+        XCTAssertEqualWithAccuracy(result.y, referenceSRGB.y, accuracy:tolerance)
+        XCTAssertEqualWithAccuracy(result.z, referenceSRGB.z, accuracy:tolerance)
+        
+    }
+    
+    func testMidpoint_SRGB_TO_LAB() {
         let colorSRGB = vector_double3(0.5, 0.5, 0.5)
         let referenceLAB = vector_double3(53.389, 0, 0)
         let result = mapSRGB_TO_LAB(colorSRGB)
@@ -46,7 +62,18 @@ class ColorSpaceConversionCIELABTests: XCTestCase {
         XCTAssertEqualWithAccuracy(result.z, referenceLAB.z, accuracy:tolerance)
     }
     
-    func testMidRed() {
+    func testMidpoint_LAB_TO_SRGB() {
+        let referenceSRGB = vector_double3(0.5, 0.5, 0.5)
+        let colorLAB = vector_double3(53.389, 0, 0)
+        let result = mapLAB_TO_SRGB(colorLAB)
+        
+        XCTAssertEqualWithAccuracy(result.x, referenceSRGB.x, accuracy:tolerance)
+        XCTAssertEqualWithAccuracy(result.y, referenceSRGB.y, accuracy:tolerance)
+        XCTAssertEqualWithAccuracy(result.z, referenceSRGB.z, accuracy:tolerance)
+    }
+    
+    
+    func testMidRed_SRGB_TO_LAB() {
         let colorSRGB = vector_double3(0.5, 0.0, 0.0)
         let referenceLAB = vector_double3(25.419, 47.910, 37.906)
         let result = mapSRGB_TO_LAB(colorSRGB)
@@ -56,7 +83,7 @@ class ColorSpaceConversionCIELABTests: XCTestCase {
         XCTAssertEqualWithAccuracy(result.z, referenceLAB.z, accuracy:tolerance)
     }
 
-    func testMidGreen() {
+    func testMidGreen_SRGB_TO_LAB() {
         let colorSRGB = vector_double3(0.0, 0.5, 0.0)
         let referenceLAB = vector_double3(46.052, -51.553, 49.756)
         let result = mapSRGB_TO_LAB(colorSRGB)
@@ -66,7 +93,7 @@ class ColorSpaceConversionCIELABTests: XCTestCase {
         XCTAssertEqualWithAccuracy(result.z, referenceLAB.z, accuracy:tolerance)
     }
     
-    func testMidBlue() {
+    func testMidBlue_SRGB_TO_LAB() {
         let colorSRGB = vector_double3(0.0, 0.0, 0.5)
         let referenceLAB = vector_double3(12.890, 47.368, -64.520)
         let result = mapSRGB_TO_LAB(colorSRGB)
@@ -76,7 +103,7 @@ class ColorSpaceConversionCIELABTests: XCTestCase {
         XCTAssertEqualWithAccuracy(result.z, referenceLAB.z, accuracy:tolerance)
     }
     
-    func testWhite() {
+    func testWhite_SRGB_TO_LAB() {
         let colorSRGB = vector_double3(1.0, 1.0, 1.0)
         let referenceLAB = vector_double3(100.000, 0, 0)
         let result = mapSRGB_TO_LAB(colorSRGB)
@@ -84,6 +111,16 @@ class ColorSpaceConversionCIELABTests: XCTestCase {
         XCTAssertEqualWithAccuracy(result.x, referenceLAB.x, accuracy:tolerance)
         XCTAssertEqualWithAccuracy(result.y, referenceLAB.y, accuracy:tolerance)
         XCTAssertEqualWithAccuracy(result.z, referenceLAB.z, accuracy:tolerance)
+    }
+    
+    func testWhite_LAB_TO_SRGB() {
+        let referenceSRGB = vector_double3(1.0, 1.0, 1.0)
+        let colorLAB = vector_double3(100.000, 0, 0)
+        let result = mapLAB_TO_SRGB(colorLAB)
+        
+        XCTAssertEqualWithAccuracy(result.x, referenceSRGB.x, accuracy:tolerance)
+        XCTAssertEqualWithAccuracy(result.y, referenceSRGB.y, accuracy:tolerance)
+        XCTAssertEqualWithAccuracy(result.z, referenceSRGB.z, accuracy:tolerance)
     }
     
 }
