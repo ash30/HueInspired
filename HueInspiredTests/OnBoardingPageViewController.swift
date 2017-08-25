c4ab738b5588ae7ea5bea4090dc19e14e62e61d0
ashley arthur <ashleyarthur98@gmail.com>
2017-08-25 20:02:32 +0100

    Onboarding VC controller tests
    Snapshot tests for different use cases of the onboarding

---
+++ HueInspiredTests/OnBoardingPageViewController.swift
@@ -0,0 +1,79 @@
+//
+//  OnBoardingPageViewController.swift
+//  HueInspired
+//
+//  Created by Ashley Arthur on 25/08/2017.
+//  Copyright © 2017 AshArthur. All rights reserved.
+//
+
+import XCTest
+
+import Foundation
+import UIKit
+import FBSnapshotTestCase
+
+@testable import HueInspired
+
+class OnboardingPageViewControllerTests: FBSnapshotTestCase {
+    
+    // MARK: HELPERS
+    
+    func setupPageViewController() -> OnBoardingPageViewController {
+        let storyBoard = UIStoryboard.init(name: "Onboarding", bundle: nil)
+        let vc = storyBoard.instantiateInitialViewController() as! OnBoardingPageViewController
+        vc.view.backgroundColor = UIColor.red
+        return vc
+    }
+    
+    func setupContentViewController(image: UIImage, title:String, blurb:String) -> OnBoardingContentViewController {
+        let storyBoard = UIStoryboard.init(name: "Onboarding", bundle: nil)
+        let vc = storyBoard.instantiateViewController(withIdentifier: "OnBoardingContentViewController") as! OnBoardingContentViewController
+        vc.image = image
+        vc.cardTitle = title
+        vc.blurb = blurb
+        return vc
+    }
+    
+
+    // MARK: LIFE CYCLE
+    
+    override func setUp() {
+        super.setUp()
+        //self.recordMode = true
+    }
+    
+    // TESTS
+    
+    func test_contentViewController(){
+        let sut = setupContentViewController(image:#imageLiteral(resourceName: "testImage512"),title:"test",blurb:"Content")
+        FBSnapshotVerifyView(sut.view)
+    }
+    
+    func test_pageViewController_withContent(){
+        let contentVC = setupContentViewController(image:#imageLiteral(resourceName: "testImage512"),title:"test",blurb:"Content")
+        let sut = setupPageViewController()
+        sut.contentViewControllers = [contentVC]
+        
+        FBSnapshotVerifyView(sut.view)
+    }
+    
+    func test_pageViewController_withActionableContent(){
+        let contentVC = setupContentViewController(image:#imageLiteral(resourceName: "testImage512"),title:"test",blurb:"Content")
+        let actionVC = ActionContainer()
+        actionVC.action = {_ in}
+        actionVC.addChildViewController(contentVC)
+        
+        let sut = setupPageViewController()
+        sut.contentViewControllers = [actionVC]
+        
+        FBSnapshotVerifyView(sut.view)
+    }
+    
+    
+
+    
+
+    
+    
+    
+}
